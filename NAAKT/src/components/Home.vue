ed7905951e6e7cfaf4a96aacef786a205f36ddcf
Pjotr <403688@student.fontys.nl>
2023-06-02 12:55:46 +0200

    12:55

---
+++ NAAKT/src/components/Home.vue
@@ -0,0 +1,24 @@
+<script setup>
+</script>
+
+<template>
+  <div id="background-img">
+    <div id="background-overlay"></div>
+  </div>
+</template>
+
+<style scoped>
+  #background-overlay{
+    background-color: #dc0404;
+    background-size: cover;
+    height: 100vh;
+    width: 100%;
+    mix-blend-mode: overlay;
+  }
+  #background-img{
+    background-image: url("../src/assets/img/home_page/home_image.png");
+    background-size: cover;
+    height: 100vh;
+    width: 100%;
+  }
+</style>
--- NAAKT/src/views/HomeView.vue
+++ NAAKT/src/views/HomeView.vue
@@ -1,32 +1,11 @@
-<template>
-  <div>
-    <h1>Data Component</h1>
-    <ul>
-      <li v-for="item in data" :key="item.id">{{ item.name }}</li>
-    </ul>
-  </div>
-</template>
-
-<script>
-import axios from 'axios';
-
-export default {
-  data() {
-    return {
-      data: []
-    };
-  },
-  mounted() {
-    // Fetch data from the server
-    axios.get('http://127.0.0.1:5173/api/data')
-  .then(response => {
-    // Handle the response
-  })
-  .catch(error => {
-    // Handle the error
-    console.error('Error fetching data:', error);
-  });
-
-  }
-};
+<script setup>
+import Home from '../components/Home.vue'
 </script>
+
+<template>
+  <main>
+    <body>
+      <Home />
+    </body>
+  </main>
+</template>
