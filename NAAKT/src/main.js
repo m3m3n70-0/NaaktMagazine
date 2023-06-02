54155953c73a5ddf33bdade5072d876265f9e96d
Max <maxnouts@gmail.com>
2023-06-02 11:37:16 +0200

    begin connection database

--- NAAKT/src/main.js
+++ NAAKT/src/main.js
@@ -1,11 +1,13 @@
-import './assets/main.css'
+import './assets/main.css';
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import axios from 'axios';
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+const app = createApp(App);
+app.use(router);
+app.mount('#app');
 
-const app = createApp(App)
-
-app.use(router)
-
-app.mount('#app')
+axios.defaults.baseURL = 'http://127.0.0.1:5173/';
+app.config.globalProperties.$http = axios;
+app.config.productionTip = false;
--- NAAKT/src/views/HomeView.vue
+++ NAAKT/src/views/HomeView.vue
@@ -1,9 +1,32 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <h1>Data Component</h1>
+    <ul>
+      <li v-for="item in data" :key="item.id">{{ item.name }}</li>
+    </ul>
+  </div>
 </template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      data: []
+    };
+  },
+  mounted() {
+    // Fetch data from the server
+    axios.get('http://127.0.0.1:5173/api/data')
+  .then(response => {
+    // Handle the response
+  })
+  .catch(error => {
+    // Handle the error
+    console.error('Error fetching data:', error);
+  });
+
+  }
+};
+</script>
