2c25f2bd539b3a26157565584e318d90a73608cf
Max <maxnouts@gmail.com>
2023-06-02 12:41:49 +0200

    start dashboard and login

--- NAAKT/src/router/index.js
+++ NAAKT/src/router/index.js
@@ -16,6 +16,16 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: () => import('../views/DashboardView.vue')
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('../views/Login.vue')
     }
   ]
 })
---
+++ NAAKT/src/views/Login.vue
@@ -0,0 +1,40 @@
+<template>
+    <div id="login">
+        <h1>Login</h1>
+        <input type="password" name="password" v-model="input.key" placeholder="Password" />
+        <button type="button" v-on:click="login()">Login</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Login',
+        data() {
+            return {
+                input: {
+                    key: "password"
+                }
+            }
+        },
+        methods: {
+            login() {
+                if(key == "password") {
+                    console.log("Login successful");
+                } else {
+                    console.log("A username and password must be present");
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    #login {
+        width: 500px;
+        border: 1px solid #CCCCCC;
+        background-color: #FFFFFF;
+        margin: auto;
+        margin-top: 200px;
+        padding: 20px;
+    }
+</style>
