6dd7490bffb550bef4d3427cc3073d198f21aef6
Max <maxnouts@gmail.com>
2023-06-05 09:21:28 +0200

    added nav links

--- NAAKT/src/App.vue
+++ NAAKT/src/App.vue
@@ -7,6 +7,8 @@ import { RouterLink, RouterView } from 'vue-router'
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/login">Login</RouterLink>
+        <RouterLink to="/dashboard">Dashboard</RouterLink>
       </nav>
   </header>
 
