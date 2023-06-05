61e8c2c41fcc1120ab6ff8c0692e66001b828460
Max <maxnouts@gmail.com>
2023-06-05 09:21:09 +0200

    Auth if login is false

--- NAAKT/src/router/index.js
+++ NAAKT/src/router/index.js
@@ -12,12 +12,33 @@ const router = createRouter({
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
-    }
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('../views/LoginView.vue')
+    },
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: () => import('../views/DashboardView.vue')
+    },
   ]
+});
+router.beforeEach((to, from, next) => {
+  // redirect to login page if not logged in and trying to access a restricted page
+  const publicPages = ['/login', '/about', '/'];
+  const authRequired = !publicPages.includes(to.path);
+  const loggedIn = localStorage.getItem('user');
+
+  if (authRequired && !loggedIn) {
+
+    console.log('authRequired && !loggedIn');
+    return next('/login');
+  }
+
+  next();
 })
 
 export default router
