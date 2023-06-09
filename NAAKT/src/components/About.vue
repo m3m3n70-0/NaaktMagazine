fd4a05036a7ca76745ac1882b179fa94178bd2fb
Pjotr <403688@student.fontys.nl>
2023-06-09 11:18:42 +0200

    about page

--- NAAKT/src/components/About.vue
+++ NAAKT/src/components/About.vue
@@ -1,6 +1,27 @@
 
 <template>
-  <div class="about-gewaagd core-values">GEWAAGD</div>
-  <div class="about-inclusief core-values">INCLUSIEF</div>  
-  <div class="about-puur core-values">PUUR</div>
+  <div class="about-gewaagd core-values"><div class="backdrop-filter">GEWAAGD</div></div>
+  <div class="about-text-container">
+    <p class="about-text">
+      De naam NAAKT komt voort uit onze visie, alles open en bloot bespreken.<br> 
+      Open en bloot is NAAKT.<br> Het is dus heel letterlijk genomen en is een kort maar krachtige naam die de aandacht wekt.
+    </p>
+  </div>
+  <div class="about-inclusief core-values"><div class="backdrop-filter">INCLUSIEF</div></div>  
+  <div class="about-text-container">
+    <p class="about-text">
+      Onze visie is een wereld waarin jongeren in staat zijn om openlijk over hun mentale welzijn te spreken, 
+      zonder taboe of stigma.<br> Wij geloven in het belang van een samenleving waar iedereen zich gehoord en gezien voelt, zonder veroordeling.<br> 
+      Een plek waar kwetsbaarheid, openheid en inclusiviteit de norm zijn.
+    </p>
+  </div>
+  <div class="about-puur core-values"><div class="backdrop-filter">PUUR</div></div>
+  <div class="about-text-container">
+    <p class="about-text">
+      Onze missie is om jongeren te inspireren,<br> 
+      informeren en uit te dagen om kritisch na te denken over zichzelf en de wereld om hen heen.<br> 
+      Door moeilijke onderwerpen bespreekbaar te maken, 
+      willen we jongeren helpen om hun volledige zelf te kunnen zijn en hun wereldbeeld te verbreden.
+    </p>
+  </div>
 </template>
